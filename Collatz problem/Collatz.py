e49c94c416d7ac0c3ddc5ca48371d2a5f08c1ff1
shimotuke1535 <simonomao1535@gmail.com>
2024-12-13 20:21:34 +0900

    コラッツ予想問題プログラムに小改造

--- Collatz problem/Collatz.py
+++ Collatz problem/Collatz.py
@@ -3,19 +3,14 @@ flag = 0
 A = int(input("Input A:"))
 while A >= 1:
     print(A)
+    if flag == 1:
+        break
     if A % 2 == 0:
         i = i + 1
         A = int(A / 2)
     else:
         i = i + 1
         A = int(3 * A + 1)
-    
     if A == 1:
         flag = flag + 1
-    
-    if flag >= 1:
-        i = i - 1
-        
-    if flag == 5:
-        break
 print("Count:",i)
